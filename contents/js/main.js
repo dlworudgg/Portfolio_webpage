d7200f566a586da861bcf92c9c6ff6bee2623451
Jae Kyeong Lee <jae@Jaes-MacBook-Pro.local>
2021-02-26 17:52:38 -0500

    scrolling down animation

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,13 +1,31 @@
 !function (a) {
     "use strict";
+    let anchorSelector = 'a[href^="#"]';
     /*Mobile menu selected to close menu*/
     $("#bs-example-navbar-collapse-1 ul li").click(function () {
         $("#bs-example-navbar-collapse-1").removeClass("in");
     });
 
+    $(anchorSelector).on('click', function (e) {
+
+        e.preventDefault();
+        let destination = $(this.hash);
+
+        let scrollPosition
+            = destination.offset().top;
+
+
+        let animationDuration = 500;
+        $('html, body').animate({
+            scrollTop: scrollPosition
+        }, animationDuration);
+    });
+
+
+
     $("#js-grid-juicy-projects").on('initComplete.cbp', function() {
     	$(".cbp-l-loadMore-button").css({"visibility": "visible", "opacity": "1", "transition-delay": "0s"});
-	});
+	}   );
 
 
 }(this);
